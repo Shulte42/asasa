115bb88ae2ebd7d049d4a6ebe24b4948b6e0019d
Shulte42 <146744333+Shulte42@users.noreply.github.com>
2023-11-10 13:08:15 -0300

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bruda-si <bruda-si@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 10:28:32 by bruda-si          #+#    #+#             */
+/*   Updated: 2023/11/10 15:53:48 by bruda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *new_line, char *buffer);
+void	*ft_nlcut(char *new_line);
+int		ft_nlhandle(char *buffer);
+int		ft_strlen(char	*buffer);
+
+#endif
