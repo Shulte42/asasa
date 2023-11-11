c103546b7bc928371f562410be8f03594370f3f0
Shulte42 <146744333+Shulte42@users.noreply.github.com>
2023-11-11 20:27:58 +0000

    Add files via upload

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bruda-si <bruda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/08 10:28:32 by bruda-si          #+#    #+#             */
-/*   Updated: 2023/11/11 15:52:58 by bruda-si         ###   ########.fr       */
+/*   Created: 2023/11/11 15:34:30 by bruda-si          #+#    #+#             */
+/*   Updated: 2023/11/11 19:21:27 by bruda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
@@ -28,8 +28,8 @@
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *new_line, char *buffer);
-void	*ft_nlcut(char *new_line);
 int		ft_nlhandle(char *buffer);
-int		ft_strlen(char	*buffer);
+int		ft_strlen(char *buffer);
+void	*ft_nlcut(char *new_line);
 
 #endif
--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -1,33 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bruda-si <bruda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/08 11:56:14 by bruda-si          #+#    #+#             */
-/*   Updated: 2023/11/11 19:42:08 by bruda-si         ###   ########.fr       */
+/*   Created: 2023/11/11 18:11:23 by bruda-si          #+#    #+#             */
+/*   Updated: 2023/11/11 19:41:59 by bruda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
-int	ft_strlen(char	*buffer)
+int ft_strlen(char *buffer)
 {
-	int	i;
-
-	i = 0;
+	int lenght;
+	
+	lenght = 0;
 	if (!buffer)
 		return (0);
-	while (buffer[i])
-		i++;
-	return (i);
+	while (buffer[lenght])
+		lenght++;
+	return (lenght);
 }
-
 void	*ft_nlcut(char *new_line)
 {
 	int	i;
-
+	
 	i = 0;
 	while (new_line[i])
 	{
@@ -42,9 +41,9 @@ void	*ft_nlcut(char *new_line)
 
 int	ft_nlhandle(char *buffer)
 {
-	int		i;
-	int		j;
-	int		nl;
+	int	i;
+	int	j;
+	int	nl;
 
 	i = 0;
 	j = 0;
@@ -53,7 +52,7 @@ int	ft_nlhandle(char *buffer)
 	{
 		if (nl == 1)
 			buffer[j++] = buffer[i];
-		if (buffer[i] == '\n')
+		if (buffer[i] == nl)
 			nl = 1;
 		buffer[i] = '\0';
 		i++;
@@ -61,24 +60,24 @@ int	ft_nlhandle(char *buffer)
 	return (nl);
 }
 
-char	*ft_strjoin(char *new_line, char *buffer)
+char    *ft_strjoin(char *new_line, char *buffer)
 {
-	int		i;
-	int		j;
-	char	*dest;
-
+	char    *dest;
+	int     i;
+	int     g;
+	
 	i = 0;
-	j = 0;
+	g = 0;
 	dest = (char *)malloc(ft_strlen(new_line) + ft_strlen(buffer) + 1);
 	if (!dest)
 		return (NULL);
-	while (new_line && new_line[i])
+	while(new_line && new_line[i])
 	{
 		dest[i] = new_line[i];
 		i++;
 	}
-	while (buffer[j])
-		dest[i++] = buffer[j++];
+	while (buffer[g])
+		dest[i++] = buffer[g++];
 	dest[i] = '\0';
 	free(new_line);
 	return (ft_nlcut(dest));
